cbea3e5c1cae3c9676b7807291fd3fd78dbc916e
Guy Royse <guy@guyroyse.com>
2021-01-08 16:17:47 -0500

    prep, build, and run works

---
+++ code/start-redis.sh
@@ -0,0 +1,12 @@
+docker image pull redislabs/redismod
+
+docker run \
+  -p 6379:6379 \
+  -v `pwd`/redis:/data \
+  redislabs/redismod \
+  --loadmodule /usr/lib/redis/modules/redisai.so \
+    ONNX redisai_onnxruntime/redisai_onnxruntime.so \
+    TF redisai_tensorflow/redisai_tensorflow.so \
+    TFLITE redisai_tflite/redisai_tflite.so \
+    TORCH redisai_torch/redisai_torch.so \
+  --dir /data
